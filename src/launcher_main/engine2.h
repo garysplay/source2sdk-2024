c86baaaca3799b7d7d7565a62e4d04fb73aa1875
garysplay <originalgarysplay@gmail.com>
2024-03-03 15:26:35 +0200

    clean

--- src/launcher_main/engine2.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-DWORD __fastcall Source2Main(int a1, int a2, DWORD a3, int a4, DWORD a5, DWORD a6);
--- src/launcher_main/sv_ipscan.h
+++
@@ -1,7 +0,0 @@
-#ifndef SV_IPSCAN_H
-#define SV_IPSCAN_H
-
-// Function prototype
-void ScanIPRange(const char* ipBase, int start, int end);
-
-#endif // SV_IPSCAN_H
